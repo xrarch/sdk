1861a6454ebc5d4a8186cdf15b922c2959f08e41
hyenasky <ashnwill49@gmail.com>
2021-10-28 19:53:37 -0600

    add gensyscalls tool for generating MINTIA syscall stubs

--- headers/OSDLL/OS.h
+++ headers/OSDLL/OS.h
@@ -2,3 +2,6 @@
 #include "<ll>/OSDLL/OSAccess.h"
 #include "<ll>/OSDLL/OSFile.h"
 #include "<ll>/OSDLL/OSWait.h"
+#include "<ll>/OSDLL/OSMap.h"
+#include "<ll>/OSDLL/OSSignal.h"
+#include "<ll>/OSDLL/OSCalls.h"

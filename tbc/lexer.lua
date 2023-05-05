fa4eb4ce904b0e5c89d4c9a0414fa2c603e0a211
hyenasky <ashnwill49@gmail.com>
2023-05-04 22:15:47 -0600

    tiny amount of work

--- tbc/lexer.lua
+++ tbc/lexer.lua
@@ -9,7 +9,19 @@ function lexer.new(filename, file, incdir, libdir, symbols)
 
 	local lex = {}
 
+	function lex.nextToken()
+		-- return a table representing a token, or nil if no next token.
 
+	end
+
+	function lex.lastToken()
+		-- un-consume the last token so that it will be returned again
+		-- by nextToken in an identical fashion to the last time it was
+		-- returned. this can only be done one step backwards in time, as the
+		-- state necessary to fetch any tokens before that has been lost by
+		-- now.
+
+	end
 
 	return lex
 end
